d55a124a56089a718f0b270a04d45fc8581bbdca
ESFA <myoptionalps@gmail.com>
2023-03-06 09:27:17 +0300

    final

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const dotenv = require('dotenv').config();
 const port = process.env.PORT || 4000
 const dbConfig = require('./config/db.config')
 const songRoute = require('./routes/songs.routes');
+const cors = require('cors');
+
 
 app.use(express.json());
+app.use(cors())
 app.use('/songs', songRoute)
 
 
